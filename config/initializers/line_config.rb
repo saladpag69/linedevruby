d7ecd697c6b6338cf6ca6223ba8906be4766250b
John Doe <zayam.info@gmail.com>
2026-03-17 14:37:59 +0700

    Remove line_config initializer

--- config/initializers/line_config.rb
+++
@@ -1,2 +0,0 @@
-LINE_CHANNEL_SECRET = ENV["LINE_CHANNEL_SECRET"] || Rails.application.credentials.line&.dig(:channel_secret)
-LINE_CHANNEL_ACCESS_TOKEN = ENV["LINE_CHANNEL_ACCESS_TOKEN"] || Rails.application.credentials.line&.dig(:channel_access_token)
