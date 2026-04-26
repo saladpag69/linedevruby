89a0b57cf494dd828f942db17aff92a30abc948e
John Doe <zayam.info@gmail.com>
2026-04-26 23:43:55 +0700

    run digitalocena but nothing happe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.assets.compile = true
 
   # Skip http-to-https redirect for the default health check endpoint.
   config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
